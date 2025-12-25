b2e2b7970db02d989e22646b2dafb7b768ff6b5d
FalaleevDanila <falaleevdanila@gmail.com>
2025-12-25 19:15:31 +0300

    add changing coordinates for apples

--- colyseus-examples/src/rooms/02-state-handler.ts
+++ colyseus-examples/src/rooms/02-state-handler.ts
@@ -8,6 +8,9 @@ export class Vector2Float extends Schema {          // Vector2Float === apple co
 
     @type("number")
     z = Math.floor(Math.random() * 256) - 128;
+
+    @type("uint32")
+    id = 0;
 }
 
 
@@ -29,9 +32,21 @@ export class State extends Schema {
     @type([Vector2Float]) 
     apples = new ArraySchema<Vector2Float>();
 
+    appleLastId = 0;
+
 
     createApple(){
-        this.apples.push(new Vector2Float());
+        const apple = new Vector2Float();
+        apple.id = this.appleLastId++;
+        this.apples.push(apple);
+        
+    }
+
+    collectApple(player: Player, data){
+        const apple = this.apples.find((value) => value.id === data.id);
+        if(apple === undefined) return;
+        apple.x = Math.floor(Math.random() * 256) - 128;
+        apple.z = Math.floor(Math.random() * 256) - 128;
     }
 
     createPlayer(sessionId: string) {
@@ -59,6 +74,12 @@ export class StateHandlerRoom extends Room<State> {
         this.onMessage("move", (client, data) => {
             this.state.movePlayer(client.sessionId, data);
         });
+
+        this.onMessage("collect", (client, data) => {
+            const player = this.state.players.get(client.sessionId);
+            this.state.collectApple(player, data);
+        });
+        
         for(let i =0; i<this.startAppleCount; i++){
             this.state.createApple();
         }
